d9f50e77a9b3db8873e8f6315758ea25b418b5cf
JonasManley <54889189+JonasManley@users.noreply.github.com>
2019-11-11 20:18:47 +0100

    Application A added - Client

---
+++ MiniProject2Client/MiniProject2Client/Form1.Designer.cs
@@ -0,0 +1,62 @@
+﻿namespace MiniProject2Client
+{
+    partial class Form1
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.labelStart = new System.Windows.Forms.Label();
+            this.SuspendLayout();
+            // 
+            // labelStart
+            // 
+            this.labelStart.AutoSize = true;
+            this.labelStart.Location = new System.Drawing.Point(114, 70);
+            this.labelStart.Name = "labelStart";
+            this.labelStart.Size = new System.Drawing.Size(35, 13);
+            this.labelStart.TabIndex = 0;
+            this.labelStart.Text = "label1";
+            this.labelStart.Click += new System.EventHandler(this.Label1_Click);
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(800, 450);
+            this.Controls.Add(this.labelStart);
+            this.Name = "Form1";
+            this.Text = "Form1";
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.Label labelStart;
+    }
+}
+
